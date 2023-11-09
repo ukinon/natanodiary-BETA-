8639f52c0bbaded9e4e5b3fd0fd281faf5fd0a72
Arfiano Jordhy <arfianoj@gmail.com>
2023-11-09 21:37:49 +0700

    add wudget section with search bar

---
+++ src/app/components/Widget.jsx
@@ -0,0 +1,18 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+export default function Widget() {
+  return (
+    <div className="xl:w-[600px] hidden lg:flex justify-center">
+      <div className="w-[90%] xl:w-[80%] sticky top-0 bg-white z-50 py-1.5">
+        <div className="flex items-center p-3 rounded-full bg-gray-200 relative">
+          <MagnifyingGlassIcon className="h-5 z-50 text-gray-500" />
+          <input
+            type="text"
+            placeholder="Search Diary"
+            className="absolute inset-0 rounded-full outline-none border-2 border-gray-300 pl-10 focus:shadow-lg bg-gray-200 focus:bg-white"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
+import Widget from "./components/Widget";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto">
       <Sidebar />
       <Feed />
+      <Widget />
     </main>
   );
 }
