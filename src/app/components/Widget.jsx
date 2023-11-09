3f7c02c3bcb1bd837c81788806fdd02905bdad08
Arfiano Jordhy <arfianoj@gmail.com>
2023-11-09 21:43:02 +0700

    update display on search

--- src/app/components/Widget.jsx
+++ src/app/components/Widget.jsx
@@ -2,7 +2,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Widget() {
   return (
-    <div className="xl:w-[600px] hidden lg:flex justify-center">
+    <div className="xl:w-[600px] hidden lg:inline ml-10">
       <div className="w-[90%] xl:w-[80%] sticky top-0 bg-white z-50 py-1.5">
         <div className="flex items-center p-3 rounded-full bg-gray-200 relative">
           <MagnifyingGlassIcon className="h-5 z-50 text-gray-500" />
