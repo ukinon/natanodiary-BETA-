3e89ac557399aad76ca2b145fc12599e9d4e60bb
Arfiano Jordhy <arfianoj@gmail.com>
2023-12-03 23:44:28 +0700

    fix add chats

--- src/components/messages/UserList.jsx
+++ src/components/messages/UserList.jsx
@@ -1,25 +1,39 @@
+"use client";
+
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { auth, db } from "../../../firebase";
 import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-async function fetchUsers() {
-  try {
-    const response = await fetch("http:localhost:3000/api/firebase-admin", {
-      cache: "no-store",
-    });
+export default function UserList() {
+  const [users, setUsers] = useState();
+  const router = useRouter();
+  const [session] = useAuthState(auth);
 
-    const data = await response.json();
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch("/api/firebase-admin", {
+          cache: "no-store",
+        });
 
-    return data;
-  } catch (err) {
-    console.log(err.message);
-  }
-}
+        const data = await response.json();
+        const users = data?.filter(
+          (user) => user.uid !== auth?.currentUser?.uid
+        );
 
-export default async function UserList() {
-  const data = await fetchUsers();
-  const users = data?.filter((user) => user.uid !== auth?.currentUser?.uid);
+        setUsers(users);
+
+        return data;
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fetchUsers();
+  }, []);
 
   async function addChat(user) {
     const combinedId =
@@ -54,7 +68,10 @@ export default async function UserList() {
     <div className="xl:ml-[300px] mb-12 sm:mb-0 xl:min-w-[200px] sm:ml-[73px] border border-l-2 border-r-2 border-gray-100 flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky items-center border-b-2 border-gray-100 justify-between bg-white top-0 z-50">
         <div className="flex flex-row gap-2 items-center">
-          <ArrowLeftIcon className="h-7 xl:h-8 hoverEffect" />
+          <ArrowLeftIcon
+            className="h-7 xl:h-8 hoverEffect"
+            onClick={() => router.back()}
+          />
           <h2 className="text-lg font-semibold cursor-pointer">
             Add New Chats
           </h2>
@@ -78,7 +95,10 @@ export default async function UserList() {
                   </div>
                 </div>
 
-                <PlusCircleIcon className="hoverEffect h-8 sm:h-12 text-blue-500 hoverEffect" />
+                <PlusCircleIcon
+                  onClick={() => addChat(users)}
+                  className="hoverEffect h-8 sm:h-12 text-blue-500 hoverEffect"
+                />
               </div>
             </>
           ))}
